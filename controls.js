ec6fb1015658cc7f4be03315c503b3ed178a24f3
Jefferson Carpenter <jeffersoncarpenter2@gmail.com>
2017-10-11 03:01:16 -0500

    Use event.key instead of deprecated event.keyIdentifier

--- controls.js
+++ controls.js
@@ -1,38 +1,38 @@
 var Controls = function (ship) {
-  const forwardKey = 'U+0048'; 
-  const backKey = 'U+0042'; 
-  const slideRightKey = 'U+0043';
-  const slideLeftKey = 'U+0041'; 
-  const turnRightKey = 'U+0046'; 
-  const turnLeftKey = 'U+0044'; 
+  const forwardKey = '8'; 
+  const backKey = '2'; 
+  const slideRightKey = '3';
+  const slideLeftKey = '1'; 
+  const turnRightKey = '6'; 
+  const turnLeftKey = '4'; 
   
-  const fireKey = 'U+0020';
+  const fireKey = ' ';
   
-  const nextWep = 'U+0047';
+  const nextWep = '7';
   
   this.keyDown = function ( event ) {
-    if(event.keyIdentifier === forwardKey)    ship.setEngine("forward",  true);
-    if(event.keyIdentifier === backKey)       ship.setEngine("backward", true);
-    if(event.keyIdentifier === slideRightKey) ship.setEngine("right",    true);
-    if(event.keyIdentifier === slideLeftKey)  ship.setEngine("left",     true);
-    if(event.keyIdentifier === turnRightKey)  ship.setEngine("tright",   true);
-    if(event.keyIdentifier === turnLeftKey)   ship.setEngine("tleft",    true);
+    if(event.key === forwardKey)    ship.setEngine("forward",  true);
+    if(event.key === backKey)       ship.setEngine("backward", true);
+    if(event.key === slideRightKey) ship.setEngine("right",    true);
+    if(event.key === slideLeftKey)  ship.setEngine("left",     true);
+    if(event.key === turnRightKey)  ship.setEngine("tright",   true);
+    if(event.key === turnLeftKey)   ship.setEngine("tleft",    true);
 
-    if(event.keyIdentifier === fireKey)       ship.fire("primary",    true);
+    if(event.key === fireKey)       ship.fire("primary",    true);
 
-    //console.log(event.keyIdentifier);
+    //console.log(event.key);
   }
 
   this.keyUp = function ( event ) {
-    if(event.keyIdentifier === forwardKey)    ship.setEngine("forward",  false);
-    if(event.keyIdentifier === backKey)       ship.setEngine("backward", false);
-    if(event.keyIdentifier === slideRightKey) ship.setEngine("right",    false);
-    if(event.keyIdentifier === slideLeftKey)  ship.setEngine("left",     false);
-    if(event.keyIdentifier === turnRightKey)  ship.setEngine("tright",   false);
-    if(event.keyIdentifier === turnLeftKey)   ship.setEngine("tleft",    false);
+    if(event.key === forwardKey)    ship.setEngine("forward",  false);
+    if(event.key === backKey)       ship.setEngine("backward", false);
+    if(event.key === slideRightKey) ship.setEngine("right",    false);
+    if(event.key === slideLeftKey)  ship.setEngine("left",     false);
+    if(event.key === turnRightKey)  ship.setEngine("tright",   false);
+    if(event.key === turnLeftKey)   ship.setEngine("tleft",    false);
     
-    if(event.keyIdentifier === fireKey)       ship.fire("primary",    false);
+    if(event.key === fireKey)       ship.fire("primary",    false);
     
-    if(event.keyIdentifier === nextWep)       ship.nextWep();;
+    if(event.key === nextWep)       ship.nextWep();;
   }
 }
